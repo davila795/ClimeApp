e6fd5daba3d49409661948a99d290bf763d4c3fb
Carlos Davila <casedaro15@gmail.com>
2021-01-30 14:37:32 +0100

    handleChange handleSubmit and error added

--- app/src/components/Form.js
+++ app/src/components/Form.js
@@ -1,12 +1,55 @@
+import React, { useState } from 'react';
+
 const Form = () => {
+
+    const [search, setSearch] = useState({
+        country: '',
+        city: ''
+    })
+
+    const { country, city } = search
+
+    const [error, setError] = useState(false)
+
+    const handleChange = (e) => {
+        setSearch({
+            ...search,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        if (country.trim() === "" || city.trim() === "") {
+            setError(true)
+            return
+        }
+
+        setError(false)
+    }
+
     return (
-        <form>
+        <form
+            onSubmit={handleSubmit}
+        >
+            {error && <p className='red darken-4 error'>Incompleted field</p>}
+
             <div className='input-field col s12'>
                 <select
                     name='country'
                     id='country'
+                    value={country}
+                    onChange={handleChange}
                 >
                     <option value=''>--Select Country--</option>
+                    <option value="US">Estados Unidos</option>
+                    <option value="MX">México</option>
+                    <option value="AR">Argentina</option>
+                    <option value="CO">Colombia</option>
+                    <option value="CR">Costa Rica</option>
+                    <option value="ES">España</option>
+                    <option value="PE">Perú</option>
                 </select>
                 <label htmlFor='country'>Country:</label>
             </div>
@@ -16,8 +59,18 @@ const Form = () => {
                     type='text'
                     name='city'
                     id='city'
+                    value={city}
+                    onChange={handleChange}
                 />
                 <label htmlFor='city'>City:</label>
+            </div>
+
+            <div className='input-field col s12'>
+                <input
+                    type='submit'
+                    value='Search'
+                    className="waves-effect waves-light btn-large btn-block yellow accent-4"
+                />
             </div>
         </form>
     );
